92127a5cac6429f2c53d6164d928e5cad58edf22
Jennifer Ferreira <jennifersouzaferreira@outlook.com>
2021-05-29 17:50:25 -0300

    test

---
+++ One/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  One
+//
+//  Created by Jennifer Ferreira on 01/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var asd: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        asd.backgroundColor = .blue
+        asd.text = "Columax"
+        
+        
+
+        
+        
+    }
+
+   
+    
+
+}
+
---
+++ One/ViewControllerOne.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerOne.swift
+//  One
+//
+//  Created by Jennifer Ferreira on 01/05/21.
+//
+
+import UIKit
+
+class ViewControllerOne: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBOutlet var but: UIView!
+}
